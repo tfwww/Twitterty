be94c099413ec9192cd67f20d9b1b9254ced6381
Winter Zhong <zhongwm.net@gmail.com>
2015-08-11 17:04:16 +0800

    Initial Commit

---
+++ Twitterty/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Twitterty
+//
+//  Created by Wunmin on 15/8/11.
+//  Copyright (c) 2015年 Wunmin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Twitterty/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  Twitterty
+//
+//  Created by Wunmin on 15/8/11.
+//  Copyright (c) 2015年 Wunmin. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
